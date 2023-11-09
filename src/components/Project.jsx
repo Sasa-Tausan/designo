dd0a12b2b57d78bc0b8af998c9ed878f17cb4afc
Sasa-Tausan <sasatausan@gmail.com>
2023-11-09 22:33:04 +0100

    finished project list on project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,23 +1,39 @@
-import { useContext } from "react";
-import MyContext from "./MyContext";
+import { useContext } from 'react';
+import MyContext from './MyContext';
 
 const Project = () => {
   const { selectedProjectPage, handleSelectedProjectPage } =
     useContext(MyContext);
 
   console.log(selectedProjectPage);
-  const { name, text, bgClass } = selectedProjectPage;
+  const { name, text, bgClass, projects } = selectedProjectPage;
   return (
-    <main className="container page-wrapper ">
+    <main className='container page-wrapper '>
       <section
-        className={`project-hero-section bg-project-hero-section ${bgClass} d-flex align-center justify-center br-15 `}
+        className={`project-hero-section bg-project-hero-section ${bgClass} d-flex align-center justify-center  `}
       >
-        <div className="project-hero-article d-flex flex-col align-center">
-          <h1 className="heading-one text-clr-white medium capitalize ">
+        <div className='project-hero-article d-flex flex-col align-center'>
+          <h1 className='heading-one text-clr-white medium capitalize '>
             {name}
           </h1>
-          <p className="body-text text-clr-white">{text}</p>
+          <p className='body-text text-clr-white'>{text}</p>
         </div>
+      </section>
+      <section className='project-grid'>
+        {projects.map((item, index) => {
+          const { img, title, cardText } = item;
+          return (
+            <a href='#' key={index} className='card '>
+              <img src={img} alt='' className='card-image' />
+              <div className='card-content'>
+                <h3 className='heading-three text-clr-peach medium ls-5 uppercase'>
+                  {title}
+                </h3>
+                <p className='body-text text-clr-dark-grey'>{cardText}</p>
+              </div>
+            </a>
+          );
+        })}
       </section>
     </main>
   );
