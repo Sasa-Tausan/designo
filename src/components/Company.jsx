dbd6ba57b9a80936452d2431389f124d89eabd0d
Sale1982 <sasatausan@gmail.com>
2023-11-10 15:05:11 +0100

    finished hero section in company page

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,9 +1,9 @@
-import { aboutImgDesktop } from "../images";
+import { aboutImgDesktop, aboutImgMobile, aboutImgTablet } from "../images";
 
 const Company = () => {
   return (
     <main className="container page-wrapper">
-      <section className="company-hero-section d-flex br-15">
+      <section className="company-hero-section d-flex">
         <article className="article-content d-flex flex-col">
           <h1 className="heading-one text-clr-white medium capitalize">
             about us
@@ -18,6 +18,8 @@ const Company = () => {
           </p>
         </article>
         <picture>
+          <source media="(max-width: 375px)" srcSet={aboutImgMobile} />
+          <source media="(max-width:768px )" srcSet={aboutImgTablet} />
           <img src={aboutImgDesktop} alt="" className="company-hero-img" />
         </picture>
       </section>
