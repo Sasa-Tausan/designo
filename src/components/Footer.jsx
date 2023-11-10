6e4febfe9d0d6ded8325a51d281676745c65e7c7
Sale1982 <sasatausan@gmail.com>
2023-11-10 10:01:10 +0100

    finished project pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import MyContext from './MyContext';
-import SocialLinks from './SocialLinks';
-import { logoLight } from '../images';
-import { menuList } from '../assets/data';
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import MyContext from "./MyContext";
+import SocialLinks from "./SocialLinks";
+import { logoLight } from "../images";
+import { menuList } from "../assets/data";
 
 const Footer = () => {
   const { path } = useContext(MyContext);
   return (
-    <footer className='bg-dark d-flex'>
-      <div className='container d-flex flex-col align-center'>
-        {/* {path !== "/contact" && (
+    <footer className="bg-dark d-flex">
+      <div className="container d-flex flex-col align-center">
+        {path !== "/contact" && (
           <div className="footer-top d-flex align-center space-between br-15">
             <div className="footer-header-content d-flex flex-col">
               <h2 className="heading-two medium text-clr-white">
@@ -28,22 +28,22 @@ const Footer = () => {
               get in touch
             </Link>
           </div>
-        )} */}
+        )}
 
-        <div className='footer-bottom d-flex flex-col'>
-          <div className='d-flex align-center space-between'>
-            <Link to='/'>
-              <img src={logoLight} alt='' className='logo' />
+        <div className="footer-bottom d-flex flex-col">
+          <div className="d-flex align-center space-between">
+            <Link to="/">
+              <img src={logoLight} alt="" className="logo" />
             </Link>
-            <hr className='mobile-divider' />
-            <nav className='footer-nav d-flex align-center'>
+            <hr className="mobile-divider" />
+            <nav className="footer-nav d-flex align-center">
               {menuList.map((item, index) => {
                 const { path, name } = item;
                 return (
                   <NavLink
                     key={index}
                     to={path}
-                    className='nav-item text-clr-white ls-2 uppercase '
+                    className="nav-item text-clr-white ls-2 uppercase "
                   >
                     {name}
                   </NavLink>
@@ -51,15 +51,15 @@ const Footer = () => {
               })}
             </nav>
           </div>
-          <hr className='divider' />
-          <div className=' d-flex align-center space-between'>
-            <div className='address-content d-flex align-center'>
-              <div className='address d-flex flex-col body-text text-clr-white opacity-50'>
+          <hr className="divider" />
+          <div className=" d-flex align-center space-between">
+            <div className="address-content d-flex align-center">
+              <div className="address d-flex flex-col body-text text-clr-white opacity-50">
                 <p>Designo Central Office </p>
                 <p>3886 Wellington Street</p>
                 <p>Toronto, Ontario M9C 3J</p>
               </div>
-              <div className='address d-flex flex-col body-text text-clr-white opacity-50'>
+              <div className="address d-flex flex-col body-text text-clr-white opacity-50">
                 <p>Designo Central Office </p>
                 <p>3886 Wellington Street</p>
                 <p>Toronto, Ontario M9C 3J</p>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,37 +1,72 @@
-import { useContext } from 'react';
-import MyContext from './MyContext';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import MyContext from "./MyContext";
+import { iconRightArrow } from "../images";
 
 const Project = () => {
   const { selectedProjectPage, handleSelectedProjectPage } =
     useContext(MyContext);
 
   console.log(selectedProjectPage);
-  const { name, text, bgClass, projects } = selectedProjectPage;
+  const { name, text, bgClass, projects, design } = selectedProjectPage;
   return (
-    <main className='container page-wrapper '>
+    <main className="container page-wrapper ">
       <section
         className={`project-hero-section bg-project-hero-section ${bgClass} d-flex align-center justify-center  `}
       >
-        <div className='project-hero-article d-flex flex-col align-center'>
-          <h1 className='heading-one text-clr-white medium capitalize '>
+        <div className="project-hero-article d-flex flex-col align-center">
+          <h1 className="heading-one text-clr-white medium capitalize ">
             {name}
           </h1>
-          <p className='body-text text-clr-white'>{text}</p>
+          <p className="body-text text-clr-white">{text}</p>
         </div>
       </section>
-      <section className='project-grid'>
+      <section className="project-grid">
         {projects.map((item, index) => {
           const { img, title, cardText } = item;
           return (
-            <a href='#' key={index} className='card '>
-              <img src={img} alt='' className='card-image' />
-              <div className='card-content'>
-                <h3 className='heading-three text-clr-peach medium ls-5 uppercase'>
+            <a href="#" key={index} className="card ">
+              <img src={img} alt="" className="card-image" />
+              <div className="card-content">
+                <h3 className="heading-three text-clr-peach medium ls-5 uppercase">
                   {title}
                 </h3>
-                <p className='body-text text-clr-dark-grey'>{cardText}</p>
+                <p className="body-text text-clr-dark-grey">{cardText}</p>
               </div>
             </a>
+          );
+        })}
+      </section>
+      <section className="project-page-section d-flex align-center space-between">
+        {design.map((item, index) => {
+          const {
+            name,
+            images: { desktop, tablet, mobile },
+          } = item;
+          return (
+            <Link
+              to="/project"
+              key={index}
+              className="project-link relative d-flex align-center justify-center"
+              onClick={() => handleSelectedProjectPage(name)}
+            >
+              <picture>
+                <source media="(max-width: 375px)" srcSet={mobile} />
+                <source media="(max-width:768px )" srcSet={tablet} />
+                <img src={desktop} alt="" className="br-15" />
+              </picture>
+              <div className=" absolute d-flex flex-col align-center">
+                <h2 className="heading-two text-clr-white medium uppercase">
+                  {name}
+                </h2>
+                <div className="d-flex align-center space-between gap-20">
+                  <p className="fs-15 text-clr-white medium uppercase ls-5">
+                    view projects
+                  </p>
+                  <img src={iconRightArrow} alt="" />
+                </div>
+              </div>
+            </Link>
           );
         })}
       </section>
